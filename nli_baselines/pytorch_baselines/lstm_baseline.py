93539ab6b6e45679b7b584acee8f24d54455ff32
Kinga Andrea Gemes <kinga.andrea.gemes@gmail.com>
2018-03-28 13:01:17 +0200

    Pytorch baseline modified

--- nli_baselines/pytorch_baselines/lstm_baseline.py
+++ nli_baselines/pytorch_baselines/lstm_baseline.py
@@ -3,7 +3,8 @@ import torch.autograd as autograd
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as functional
-#import gensim
+import gensim
+import time
 
 torch.manual_seed(1)
 
@@ -24,9 +25,11 @@ class LSTMBaseline(nn.Module):
         """
         super(LSTMBaseline, self).__init__()
         self.hidden_dim = hidden_dim
-        #self.word_embedding = gensim.models.KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True, limit=vocab_len*2)
-        self.embedding = nn.Embedding(vocab_len, embedding_dim)
-        #self.embedding.weight = nn.Parameter(self.word_embedding)
+        self.word_embedding = gensim.models.KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin',
+                                                                              binary=True, limit=vocab_len*2)
+        self.word_dict = {w.lower(): i for i, w in enumerate(self.word_embedding.vocab)}
+        self.embedding = nn.Embedding(vocab_len*3, embedding_dim)
+        self.embedding.weight.data[0:vocab_len*2, 0:embedding_dim] = torch.from_numpy(self.word_embedding.syn0)
         self.lstm = nn.LSTM(embedding_dim, hidden_dim)
         self.linear = nn.Linear(hidden_dim, output_size)
         self.hidden = (autograd.Variable(torch.zeros(1, 1, self.hidden_dim)),
@@ -34,17 +37,21 @@ class LSTMBaseline(nn.Module):
         self.loss_function = nn.NLLLoss()
         self.optimizer = optim.SGD(self.parameters(), lr=0.1)
 
-    def prepare_sequence(self, sequence, to_ix):
+    def prepare_sequence(self, sequence):
         """
-        The function that translates the words in the sequence to the number that it belongs to in the to_ix.
+        The function that translates the words in the sequence to the number that it belongs to in the word embedding.
         :param sequence: the sequence we want to translate
         :type sequence: list
-        :param to_ix: the lookup table of word to number translations
-        :type to_ix: dict
         :return: the vector of the translation
         :rtype autograd Variable
         """
-        idxs = [to_ix[w] for w in sequence]
+        idxs = []
+        for w in sequence:
+            if w in self.word_dict:
+                idxs.append(self.word_dict[w])
+            else:
+                self.word_dict[w] = len(self.word_dict)
+                idxs.append(self.word_dict[w])
         tensor = torch.LongTensor(idxs)
         return autograd.Variable(tensor)
 
@@ -62,20 +69,18 @@ class LSTMBaseline(nn.Module):
                                           self.hidden)
         lstm_out2, self.hidden = self.lstm(self.embedding(sentence_premise).view(len(sentence_premise), 1, -1),
                                            self.hidden)
-        tag_space = self.linear(torch.add(lstm_out.view(len(sentence_hypothesis), -1)[-1],
-                                          lstm_out2.view(len(sentence_premise), -1)[-1]))
+        tag_space = self.linear(lstm_out.view(len(sentence_hypothesis), -1)[-1] +
+                                lstm_out2.view(len(sentence_premise), -1)[-1])
         tag_scores = functional.log_softmax(tag_space, dim=0)
         return tag_scores
 
-    def back_propagation(self, epochs, training_data, word_to_ix, tag_to_ix):
+    def back_propagation(self, epochs, training_data, tag_to_ix):
         """
         Backward propagation of the data and updating the network
         :param epochs: how many times we want to propagate with the training data
         :type epochs: int
         :param training_data: the sentences and expected tags that we use for training
         :type training_data: list
-        :param word_to_ix: the lookup table of the words
-        :type word_to_ix: dict
         :param tag_to_ix: the lookup table of the tags
         :type tag_to_ix: dict
         :return: None
@@ -88,8 +93,8 @@ class LSTMBaseline(nn.Module):
 
                 # Step 2. Get our inputs ready for the network, that is, turn them into
                 # Variables of word indices.
-                sentence_h = self.prepare_sequence(sentence_hypothesis, word_to_ix)
-                sentence_p = self.prepare_sequence(sentence_premise, word_to_ix)
+                sentence_h = self.prepare_sequence(sentence_hypothesis)
+                sentence_p = self.prepare_sequence(sentence_premise)
                 target = tag_to_ix[tag]
 
                 # Step 3. Run our forward pass.
